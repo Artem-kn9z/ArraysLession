71e19237d506f02d6d56b04b87ef421890411d67
Artem <pupok1015@mail.ru>
2025-04-11 15:09:22 +0300

    More switch!!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,13 +7,24 @@ public class Main {
         boolean flag = true;
         boolean found;
 
+        Book book1 = new Book("J","K",1999);
+        Book book2 = new Book("F","M",1999);
+        Book book3 = new Book("A","N",1999);
+        Book book4 = new Book("C","L",1999);
+
+        books.add(book1);
+        books.add(book2);
+        books.add(book3);
+        books.add(book4);
+
         while (flag) {
 
             System.out.println("1) Добавить книгу \n" +
                                 "2) Посмотреть все книги в списке \n" +
-                                 "3) Удалить книгу \n" +
-                                  "4) Поиск книги \n" +
-                                   "5) Выход");
+                                 "3) Отсортировать книги по названию \n" +
+                                  "4) Удалить книгу \n" +
+                                   "5) Поиск книги \n" +
+                                    "6) Выход");
 
             String choice = sc.nextLine();
 
@@ -47,12 +57,43 @@ public class Main {
                     break;
 
                 case "3":
+
+                    System.out.println("Выбери по чём сортируем:");
+                    System.out.println("1) По названию. \n" +
+                                        "2) По автору. \n" +
+                                         "3) По году издания?");
+                    switch (sc.nextInt()) {
+                        case 1:
+                            Collections.sort(books);
+                            break;
+                        case 2:
+                            Collections.sort(books, new BookAuthorComparator());
+                            break;
+                        case 3:
+                            Collections.sort(books, new BookYOPComparator());
+                            break;
+
+                        default:
+                            System.out.println("1, 2 или 3 ебобо!");
+                            break;
+                    }
+
+                    if(books.isEmpty()){
+                        System.out.println("Список книг пуст.");
+                    }else {
+                        for (int i = 0; i < books.size(); i++) {
+                            System.out.println((i+1) + ") " + books.get(i));
+                        }
+                    }
+                    break;
+
+                case "4":
                     System.out.println("Напиши название книги которую ты хочешь удалить со списка: ");
                     String titleToRemove = sc.nextLine();
                     found = false;
 
                     for (int i = 0; i < books.size(); i++) {
-                        if (books.get(i).getTitle().equals(titleToRemove)) {
+                        if (books.get(i).getTitle().equalsIgnoreCase(titleToRemove)) {
                             books.remove(i);
                             System.out.println("Книга удалена");
                             found = true;
@@ -65,7 +106,7 @@ public class Main {
                     }
                     break;
 
-                case "4":
+                case "5":
                         System.out.println("Какую книгу ты хочешь натйти?");
                         String searchBook = sc.nextLine();
                         found = false;
@@ -82,7 +123,7 @@ public class Main {
                         }
                     break;
 
-                case "5":
+                case "6":
                     flag = false;
                     break;
 
